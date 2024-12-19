9628f4d1884e1d9911a2fbc357fda0a2bdc6e33c
Frankie <frankiemichael96@gmail.com>
2024-12-19 20:13:03 +0000

    Updates

--- src/components/AppBtn.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <button class="font-bold py-2 rounded">
-    <slot></slot>
-  </button>
-</template>
--- src/components/pages/HomePage.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import MyDetails from '@/components/MyDetails.vue';
-import AppBtn from '@/components/AppBtn.vue';
-</script>
-
-<template>
-  <div class="wrapper flex items-center justify-center h-full"></div>
-</template>
